9f4151ebc149fffcf48858ae7c818822ac138cbf
jinjianming <17610798390@163.com>
2020-07-23 16:34:12 +0800

    new - nginx

--- stable/nginx/install.sh
+++
@@ -1,5 +0,0 @@
-if [[ -z $1 ]]; then
-        echo "USAGE: sh $0 values.yaml"
-else
-        helm install -f $1 --name nginx-poc  .
-fi
--- stable/nginx/upgrade.sh
+++
@@ -1,5 +0,0 @@
-if [[ -z $1 ]]; then
-        echo "USAGE: sh $0 values.yaml"
-else
-        helm upgrade nginx-poc --values $1 .
-fi
